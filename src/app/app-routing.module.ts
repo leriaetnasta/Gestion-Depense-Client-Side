8e63d43579d440b0e4295b46370fc61ddd726f50
TALHA_LOUBNA <talha_loubna@emsi-edu.ma>
2022-08-28 19:53:29 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DepensesComponent } from './depenses/depenses.component';
+import {MotifsComponent} from "./motifs/motifs.component";
+import {EmployesComponent} from "./employes/employes.component";
+import {DeplacementsComponent} from "./deplacements/deplacements.component";
+import {ProjetsComponent} from "./projets/projets.component";
+import {ClientsComponent} from "./clients/clients.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path : 'depenses', component : DepensesComponent},
+  {path : 'motifs' , component : MotifsComponent},
+  {path : 'employes', component : EmployesComponent},
+  {path : 'deplacements', component : DeplacementsComponent},
+  {path : 'projets', component : ProjetsComponent},
+  {path : 'clients', component : ClientsComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DepensesComponent } from './depenses/depenses.component';
+import { MotifsComponent } from './motifs/motifs.component';
+import { DeplacementsComponent } from './deplacements/deplacements.component';
+import { ProjetsComponent } from './projets/projets.component';
+import { ClientsComponent } from './clients/clients.component';
+import { EmployesComponent } from './employes/employes.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DepensesComponent,
+    MotifsComponent,
+    DeplacementsComponent,
+    ProjetsComponent,
+    ClientsComponent,
+    EmployesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
