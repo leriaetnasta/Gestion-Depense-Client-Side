f0c2b2faf1b94b5e1c646df9c9da751b518c0a4b
TALHA_LOUBNA <talha_loubna@emsi-edu.ma>
2022-09-04 19:12:41 +0100

    employe changes

--- src/app/employes/employes.component.ts
+++ src/app/employes/employes.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {EmployeService} from "../services/employe.service";
+import {Observable} from "rxjs";
+import {Employe} from "../model/employe.model";
 
 @Component({
   selector: 'app-employes',
@@ -6,10 +10,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employes.component.css']
 })
 export class EmployesComponent implements OnInit {
-
-  constructor() { }
+  employeFormGroup!:FormGroup;
+  currentPage:number=0;
+  size:number=5;
+  employeObservable$!:Observable<Employe>;
+  constructor(private fb:FormBuilder, private employeService:EmployeService) { }
 
   ngOnInit(): void {
+    this.employeFormGroup=this.fb.group(
+        {
+          employeId: this.fb.control('')
+        }
+    )
   }
 
+  handleSearchEmploye() {
+    let empId:number=this.employeFormGroup.value.employeId;
+    this.employeObservable$=this.employeService.searchEmploye(empId,this.currentPage,this.size);
+
+  }
 }
---
+++ src/app/model/employe.model.ts
@@ -0,0 +1,9 @@
+export interface Employe {
+    id:          number;
+    nom:         string;
+    prenom:      string;
+    email:       string;
+    departement: string;
+    phoneNumber: string;
+    matricule:   string;
+}
