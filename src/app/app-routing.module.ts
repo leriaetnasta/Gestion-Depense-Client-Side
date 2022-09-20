80f31b6278fe4a853ebec78d00702a6f281a3865
TALHA_LOUBNA <talha_loubna@emsi-edu.ma>
2022-09-20 22:59:05 +0100

    add-ons

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DepensesComponent } from './depenses/depenses.component';
-import {MotifsComponent} from "./motifs/motifs.component";
 import {EmployesComponent} from "./employes/employes.component";
 import {DeplacementsComponent} from "./deplacements/deplacements.component";
 import {ProjetsComponent} from "./projets/projets.component";
 import {ClientsComponent} from "./clients/clients.component";
-import {AddClientComponent} from "./add-client/add-client.component";
+import {LoginComponent} from "./login/login.component";
 
 
 const routes: Routes = [
   {path : 'depenses', component : DepensesComponent},
-  {path : 'motifs' , component : MotifsComponent},
   {path : 'employes', component : EmployesComponent},
   {path : 'deplacements', component : DeplacementsComponent},
   {path : 'projets', component : ProjetsComponent},
   {path : 'clients', component : ClientsComponent},
-  {path : 'addclient', component : AddClientComponent}
+  {path : 'login', component : LoginComponent}
+
 
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,33 +6,40 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { DepensesComponent } from './depenses/depenses.component';
-import { MotifsComponent } from './motifs/motifs.component';
 import { DeplacementsComponent } from './deplacements/deplacements.component';
 import { ProjetsComponent } from './projets/projets.component';
 import { ClientsComponent } from './clients/clients.component';
 import { EmployesComponent } from './employes/employes.component';
-import {ReactiveFormsModule} from "@angular/forms";
-import { AddClientComponent } from './add-client/add-client.component';
-
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './login/login.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     DepensesComponent,
-    MotifsComponent,
     DeplacementsComponent,
     ProjetsComponent,
     ClientsComponent,
     EmployesComponent,
-    AddClientComponent
+    LoginComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    ReactiveFormsModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        ReactiveFormsModule,
+        NgbModule,
+        FormsModule,
+        MDBBootstrapModule.forRoot(),
+        BrowserAnimationsModule,
+    ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [NO_ERRORS_SCHEMA]
+
 })
 export class AppModule { }
